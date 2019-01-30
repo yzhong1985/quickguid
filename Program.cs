55c749268c6b400e1b482db9c91b1d5c8c15941d
Yang Zhong <yzhong1985@hotmail.com>
2019-01-30 14:57:28 -0800

    project structure

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Windows.Forms;
+
+class Program
+{
+    [STAThread()]
+    static void Main(string[] args)
+    {
+        Clipboard.Clear();
+        Clipboard.SetText(Guid.NewGuid().ToString());
+    }
+}
